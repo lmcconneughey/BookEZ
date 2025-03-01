7b675b2aa66183d4eb15b0904c7257544db11cad
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-03-01 07:50:57 -0500

    feat: add useContext, we will keep track of cart data throughout store and at static nav-bar

---
+++ book-ez/src/app/provider/index.js
@@ -0,0 +1,15 @@
+'use client'//managing and maintaining state
+
+import { useState } from "react"
+import { StoreContext } from "../context"
+
+
+export default function StoreProvider({children}) {
+    const [cartData, setCartData] = useState([]);
+
+    return(
+        <StoreContext.Provider value={{cartData, setCartData}} >
+            {children}
+        </StoreContext.Provider>  
+    )
+}
