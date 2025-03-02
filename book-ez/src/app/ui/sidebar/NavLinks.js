7e15a101b5ea7ed4aa9b909d465a9e629ce3622c
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-03-02 11:27:27 -0500

    fix: corrected typo in nav bar href for track order

--- book-ez/src/app/ui/sidebar/NavLinks.js
+++ book-ez/src/app/ui/sidebar/NavLinks.js
@@ -22,7 +22,7 @@ const links = [
     {name: 'Books', href: '/store', icon: BookOpenIcon},
     {name: 'Sell or Lend', href: '/store/sell-lend', icon: RectangleStackIcon},
     {name: 'Cart', href: '/store/cart', icon: ShoppingCartIcon},
-    {name: 'Track Order', href: '/store/track', icon: TruckIcon},
+    {name: 'Track Order', href: '/store/track-order', icon: TruckIcon},//<<updated href
     {name: 'Community', href: '/store/community', icon: UserGroupIcon},
     {name: 'Settings', href: '/store/settings', icon: CogIcon},
     {name: 'About', href: '/store/about', icon: Squares2X2Icon},
@@ -41,10 +41,11 @@ const NavLinks = () => {
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-500 p-3 text-sm font-medium hover:bg-sky-100 hover:text-purple-600 md:flex-none md:justify-start md:p-2 md:px-3"
-            > 
-            <IconComponent className="w-6" />{/** dor every icon in our array, a component is rendered */}
+            >
+            <IconComponent className="w-6" />{/** for every icon in our array, a component is rendered */}
             {(link.name === 'Cart' && cartData && cartData.length > 0) 
-             ? `${link.name}(${cartData.length})` : <p className="hidden md:block">{link.name}</p>}
+             ? `(${cartData.length}) in ` : <p className="hidden md:block">{link.name}</p>}
+              {link.name}
             </Link>
             
         )
