2ed562aa5ad72bd86b58a9e0775cec9f725e5fef
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-03-03 02:50:09 -0500

    feat: add user auth with google and github

---
+++ book-ez/src/app/store/login/page.js
@@ -0,0 +1,15 @@
+import LoginForm from "../../ui/log-in-out-form/LoginForm"
+
+const LogIn = () => {
+    return(
+        <div>
+            <div className="text-center text-gray-500">
+                Log In
+            </div>
+            <LoginForm />
+        </div>  
+    )
+}
+
+export default LogIn
+
