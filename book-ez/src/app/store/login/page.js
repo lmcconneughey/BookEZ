6a18b2780427748a390874f79e281e1cb873e342
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-03-03 16:49:29 -0500

    chore: formating

--- book-ez/src/app/store/login/page.js
+++ book-ez/src/app/store/login/page.js
@@ -1,15 +1,23 @@
-import LoginForm from "../../ui/log-in-out-form/LoginForm"
+import Link from "next/link";
+import LoginForm from "../../ui/log-in-out-form/LoginForm";
 
 const LogIn = () => {
-    return(
-        <div>
-            <div className="text-center text-gray-500">
-                Log In
-            </div>
-            <LoginForm />
-        </div>  
-    )
-}
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-center text-3xl my-3 text-gray-500">Log In</div>
+      <div >
+        <LoginForm />
+      </div>
+      <div>
+        <p className="text-center text-3xl my-3 text-gray-500">
+          Not Registered?
+        </p>
+        <Link href="/store/register" className="mx-2 underline flex flex-col justify-center items-center ">
+          Register
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default LogIn
-
+export default LogIn;
--- book-ez/src/app/ui/log-in-out-form/LoginForm.js
+++ book-ez/src/app/ui/log-in-out-form/LoginForm.js
@@ -22,9 +22,12 @@ const LoginForm = () => {
                         id="password"
                     />
                 </div>
-                <button type="submit" className="min-w-32  rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
-                    Login with Credentials
-                </button>
+                <div className="flex flex-col justify-center">
+                    <button type="submit" className="min-w-32 rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">
+                        Login with Credentials
+                    </button>
+                </div>
+                
             </form>
             <SocialLogin />
         </div>
