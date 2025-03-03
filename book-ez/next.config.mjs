db12e227a80f1b2cc098ebf2cd1da84de8e67381
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-03-03 03:02:11 -0500

    feat: add updated to view user images from socialauth

--- book-ez/next.config.mjs
+++ book-ez/next.config.mjs
@@ -2,10 +2,17 @@
 const nextConfig = {
     images: {
         remotePatterns: [
-          {
+          {// for db images
             protocol: 'https',
-            hostname: 'res.cloudinary.com',
-    
+            hostname: 'res.cloudinary.com',    
+          },
+          {//added for google user images
+            protocol: 'https',
+            hostname: 'lh3.googleusercontent.com',     
+          },
+          {//added for github user images
+            protocol: 'https',
+            hostname: 'avatars.githubusercontent.com',     
           },
         ],
       },
