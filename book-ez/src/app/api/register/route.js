6930215fcee459687f3e0429847b35ad167c5884
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-03-04 10:12:23 -0500

    feat: add user data registration now connects to dbwith hashed pw

--- book-ez/src/app/api/register/route.js
+++ book-ez/src/app/api/register/route.js
@@ -1,17 +1,33 @@
 import { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { createUser } from "../../../db/queries";
+import dbConnect from "../../../services/mongo";
+import bcrypt from "bcryptjs"
 
 export const POST = async (request) => {
     const {name, email, password} = await request.json()//<< the names on our registration form
     console.log(name, email, password);
     
-    // [] Create DB Connection Obj
+    // [x] Create DB Connection Obj
+    await dbConnect()
+    // [x] Encrypt Password
+    const hashedPassword = await bcrypt.hash(password, 5)
+    // [x] Form DB Payload
+    const newUser = {
+       name,
+       password: hashedPassword, //<< password is changed to hashed password  
+       email
+    }
+    // [x] Update DB
+    try {
+        await createUser(newUser)
+    } catch (error) {
+        return new NextResponse(error.message, {
+            status: 500,
+        })
+    }
+    
 
-    // [] Encrypt Password
-
-    // [] Form DB Payload
-
-    // [] Update DB
     return new NextResponse("User has been created", {
         status: 201
     })
