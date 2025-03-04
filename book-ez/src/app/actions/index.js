6e38d85ff0d5e17edeb43dbf9cd72dc845965a2a
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-03-04 14:38:12 -0500

    feat: add server actions for credentials login

--- book-ez/src/app/actions/index.js
+++ book-ez/src/app/actions/index.js
@@ -12,3 +12,16 @@ export async function doSocialLogin(formData) {
 export async function doLogout(formdata) {//<< for logout, no action needed!
     await signOut({redirectTo: "/store"})
 }
+// server action for authorize
+export async function doCredentialLogin(formData) {
+    try {
+        const response = await signIn("credentials", {
+            email: formData.get('email'),
+            password: formData.get('password'),
+            redirect: false//<< incase of error, will handle redirect at login form
+        });
+        return response
+    } catch (error) {
+        throw new Error(error.message)
+    }
+}
