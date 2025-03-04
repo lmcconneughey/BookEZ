3135f410ace2dd4f7c2e8803e115d2dea4245b67
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-03-04 10:50:39 -0500

    chore: commented out dup db connection, we have a connection at  /services/mongo.js

---
+++ book-ez/src/app/lib/mongo.js
@@ -0,0 +1,11 @@
+// we'll make the connection to our DB
+// import mongoose, { mongo } from "mongoose";
+
+// export async function dbConnect () {
+//   try {
+//     const conn = await mongoose.connect(String(process.env.MONGO_DB_CONNECTION_STRING))
+//     return conn
+//   } catch (error) {
+//     throw new Error(error)
+//   }
+// }
