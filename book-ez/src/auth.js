9b2999fe69825e78ecb703539bd73686baedb5e2
lawrence mcconneughey <lmcconneughey@gmail.com>
2025-03-04 14:27:14 -0500

    feat: add credentials provider for user signin

--- book-ez/src/auth.js
+++ book-ez/src/auth.js
@@ -2,6 +2,11 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
 
+import CredentialsProvider from "next-auth/providers/credentials"
+import { User } from "./models/user-model";
+import bcrypt from "bcryptjs"
+//authenticate using register data/mongoDB
+
 
 export const {
     handlers: { GET, POST },
@@ -9,7 +14,39 @@ export const {
     signIn,
     signOut
 } = NextAuth({
+    session: {
+        strategy: "jwt",
+    },
     providers: [
+        CredentialsProvider({
+            async authorize(credentials) {
+                if (credentials === null) return null;
+
+                try {
+                    const user = await User.findOne({
+                        email: credentials?.email//<< if email: match credentials.email => user is found
+                    })
+                    
+                    if(user) {
+                        const isMatch = await bcrypt.compare(
+                            credentials.password,
+                            user.password
+                        )
+                        console.log(isMatch);
+                        
+                        if(isMatch) {
+                            return user
+                        } else {
+                            throw new Error("Check your password!") 
+                        }
+                    } else {
+                        throw new Error("user not found")
+                    }
+                } catch (error) {
+                    throw new Error(error.message)
+                }
+            }
+        }),
         Google({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
